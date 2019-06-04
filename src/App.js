20361b49d9c1fa7b7fe8bc897c12902ddbfceab4
Kevin Haiduy Phung <kphung29@gmail.com>
2019-06-03 22:25:03 -0400

    Added PageNotFound component for broken links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import AboutPage from "./components/about/AboutPage";
 import Header from "./components/common/Header";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
